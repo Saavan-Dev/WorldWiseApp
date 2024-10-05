3a79f1729b8259d115093afdd8548d9b3cd00532
Saavan-Dev <crosslpdgajjar@gmail.com>
2024-10-05 20:50:57 +0530

    correction to the base URL

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -5,7 +5,7 @@ import {
   useEffect,
   useReducer,
 } from "react";
-const BASE_URL = import.meta.env.REACT_APP_API_URL;
+const BASE_URL = "http://localhost:8000";
 
 const CitiesContext = createContext();
 
